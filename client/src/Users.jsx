ab44f05e92747695b159acf23da3279c221e9238
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-06-14 12:59:53 +0530

    Update Users.jsx

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
 
 const Users = () =>
 {
@@ -8,6 +9,7 @@ const Users = () =>
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
           <div className='w-50 bg-white rounded p-3'>
+              <Link to='/create' className= 'btn btn-success'>Add +</Link>
               <table className='table'>
                   <thead>
                       <tr>
