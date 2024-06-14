ec0aec7dfe8f94772b4fe62b8b190f3aaf847079
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-06-14 21:50:03 +0530

    Add deleting of entries code

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,40 +1,59 @@
-import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Users = () =>
 {
-    const [Users, setUsers] = useState([{
-        Name: "Sarthak", Email: "sar@gmail.com", Age: 20
-    }]) 
-  return (
-    <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
-          <div className='w-50 bg-white rounded p-3'>
-              <Link to='/create' className= 'btn btn-success'>Add +</Link>
-              <table className='table'>
-                  <thead>
-                      <tr>
-                          <th>Name</th>
-                          <th>Email</th>
-                          <th>Age</th>
-                          <th>Action</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      {
-                          Users.map((user) =>
-                          {
-                              return <tr>
-                                  <td>{user.Name}</td>
-                                  <td>{user.Email}</td>
-                                  <td>{user.Age}</td>
-                                  <td><button>Edit</button><button>Delete</button></td>
+    const [Users, setUsers] = useState([])
+
+    useEffect(() =>
+    {
+        axios.get('http://localhost:3001/')
+            .then(result => setUsers(result.data))
+            .catch(err => console.log(err))
+    }, [])
+
+    const handleDelete = (id) =>
+    {
+        axios.delete('http://localhost:3001/deleteUser/' + id)
+            .then(result =>
+            {
+                console.log(result);
+                window.location.reload()
+            })
+            .catch(err => console.log(err))
+    }
+    return (
+        <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+            <div className='w-50 bg-white rounded p-3'>
+                <Link to='/create' className='btn btn-success'>Add +</Link>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Age</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            Users.map((user) =>
+                            {
+                                return <tr>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.age}</td>
+                                    <td>
+                                        <Link to={`/update/${user._id}`} className='btn btn-success'>Update</Link>
+                                        <button className='btn btn-danger' onClick={(e) => handleDelete(user._id)}> Delete</button></td>
                               </tr>
                           })
                       }
-                  </tbody>
-              </table>
-      </div>
-    </div>
+                </tbody>
+            </table>
+        </div>
+    </div >
   )
 }
 
